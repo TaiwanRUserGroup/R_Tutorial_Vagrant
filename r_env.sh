2cc6751d30d141e128e57d25845e61eb76c61c55
Wush Wu <wush@bridgewell.com>
2014-07-25 02:57:56 +0800

    refactor: move systemwise installation to system_wide.sh debug: correctly install R packages devtools now

--- r_env.sh
+++ r_env.sh
@@ -1,9 +1,13 @@
-apt-get install -y r-base
-apt-get install -y gdebi-core libapparmor1
 RSTUDIO_VER="0.98.977"
+RPKG_LIST="bitops RCurl httr digest memoise whisker stringr evaluate devtools"
 wget http://download2.rstudio.org/rstudio-server-$RSTUDIO_VER-amd64.deb --no-verbose
 gdebi -n -q rstudio-server-$RSTUDIO_VER-amd64.deb
 rm rstudio-server-$RSTUDIO_VER-amd64.deb
 echo "options(repos=structure(c(CRAN='http://cran.csie.ntu.edu.tw/', omegahat='http://www.omegahat.org/R')))" > /home/vagrant/.Rprofile
 su vagrant -c "Rscript -e \"dir.create(Sys.getenv('R_LIBS_USER'), recursive = TRUE)\""
-su vagrant -c "Rscript -e \"install.packages('devtools', quiet=FALSE, verbose=TRUE)\""
+for pkg in $RPKG_LIST
+do
+  su vagrant -c "Rscript -e \"download.packages('$pkg', destdir='/home/vagrant', quiet = TRUE)\""
+  su vagrant -c "cd ~ && R CMD INSTALL $pkg_*.tar.gz"
+  su vagrant -c "cd ~ && rm $pkg_*.tar.gz"
+done
--- system_wide.sh
+++ system_wide.sh
@@ -1,15 +1,17 @@
 # change apt server
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i 's#http://archive.ubuntu.com#ftp://free.nchc.org.tw#' /etc/apt/sources.list
+bash -c "echo \"deb http://cran.csie.ntu.edu.tw/bin/linux/ubuntu trusty/\" >> /etc/apt/sources.list"
 # http://archive.ubuntu.com/ubuntu
 # ftp://free.nchc.org.tw/ubuntu
 
 # add DNS server
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update
 # change timezone info
 echo 'Asia/Taipei' > /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt-get install -y git-core libcurl4-openssl-dev
+apt-get install -y git-core libcurl4-openssl-dev r-base gdebi-core libapparmor1
