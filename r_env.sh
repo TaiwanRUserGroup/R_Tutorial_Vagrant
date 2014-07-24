d81ec872743524a6787ce45c29168eaf5fb8557e
Wush Wu <wush@bridgewell.com>
2014-07-24 21:29:49 +0800

    1. remove progress bar of gdebi and wget 2. set default repository for R 3. install devtools(but failed...)

--- r_env.sh
+++ r_env.sh
@@ -1,6 +1,9 @@
 apt-get install -y r-base
 apt-get install -y gdebi-core libapparmor1
 RSTUDIO_VER="0.98.977"
-wget http://download2.rstudio.org/rstudio-server-$RSTUDIO_VER-amd64.deb
-gdebi -n rstudio-server-$RSTUDIO_VER-amd64.deb
+wget http://download2.rstudio.org/rstudio-server-$RSTUDIO_VER-amd64.deb --no-verbose
+gdebi -n -q rstudio-server-$RSTUDIO_VER-amd64.deb
 rm rstudio-server-$RSTUDIO_VER-amd64.deb
+echo "options(repos=structure(c(CRAN='http://cran.csie.ntu.edu.tw/', omegahat='http://www.omegahat.org/R')))" > /home/vagrant/.Rprofile
+su vagrant -c "Rscript -e \"dir.create(Sys.getenv('R_LIBS_USER'), recursive = TRUE)\""
+su vagrant -c "Rscript -e \"install.packages('devtools', quiet=FALSE, verbose=TRUE)\""
