4451aed8d728ecd059263f067af2acf44711d829
Wush Wu <wush@bridgewell.com>
2014-07-24 16:03:31 +0800

    add DNS server manually

--- system_wide.sh
+++ system_wide.sh
@@ -3,8 +3,11 @@ cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i 's#http://archive.ubuntu.com#ftp://free.nchc.org.tw#' /etc/apt/sources.list
 # http://archive.ubuntu.com/ubuntu
 # ftp://free.nchc.org.tw/ubuntu
-apt-get update
 
+# add DNS server
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+apt-get update
 # change timezone info
 echo 'Asia/Taipei' > /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
