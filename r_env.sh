6084331ded08c35e7f04e7302c3fcaa309f97666
dboyliao <sk413025@gmail.com>
2014-08-24 01:13:55 +0800

    edit rserver amd64 to i386

--- r_env.sh
+++ r_env.sh
@@ -1,7 +1,7 @@
 RSTUDIO_VER="0.98.977"
-wget http://download2.rstudio.org/rstudio-server-$RSTUDIO_VER-amd64.deb --no-verbose
-gdebi -n -q rstudio-server-$RSTUDIO_VER-amd64.deb
-rm rstudio-server-$RSTUDIO_VER-amd64.deb
+wget http://download2.rstudio.org/rstudio-server-$RSTUDIO_VER-i386.deb --no-verbose
+gdebi -n -q rstudio-server-$RSTUDIO_VER-i386.deb
+rm rstudio-server-$RSTUDIO_VER-i386.deb
 echo "options(repos=structure(c(CRAN='http://cran.csie.ntu.edu.tw/', omegahat='http://www.omegahat.org/R')))" > /home/vagrant/.Rprofile
 su vagrant -c "Rscript -e \"dir.create(Sys.getenv('R_LIBS_USER'), recursive = TRUE)\""
 su vagrant -c "Rscript -e \"install.packages('devtools', quiet = TRUE, verbose = FALSE)\""
